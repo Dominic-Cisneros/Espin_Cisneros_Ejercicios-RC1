1422103b709b19317b6f49c010ffa033ff509d45
Christopher-Espin <christopher.espin@udla.edu.ec>
2026-05-26 14:27:34 -0500

    Comentario bb

--- Christopher_Cisneros_EjercicioRC1.c
+++ Christopher_Cisneros_EjercicioRC1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
+//Eres lindo por ser programador, no te preocupes por el error, es normal, lo importante es aprender de él y seguir adelante. ¡Ánimo!
 int main() {
     printf("Hola Mundo\n");
     return 0;
+
 }
